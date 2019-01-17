0b1b8e564aa42e068df6a3300f18fb2249432ac4
Rexhino <rexhino.re@gmail.com>
2019-01-17 18:39:13 +0100

    refactoring

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url to connect with mongodb database
-    url: 'mongodb://rexhino:qwerty123@ds235328.mlab.com:35328/storybooks-dev'
+    url: // url to connect with mongodb database here
 }
